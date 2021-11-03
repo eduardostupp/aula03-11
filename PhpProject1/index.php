6aeed5f85fced42fa129d28413961cdc75ff4a37
eduardo.stupp <eduardo.stupp@unidavi.edu.br>
2021-11-03 20:44:11 -0300

    primeiro commit

---
+++ PhpProject1/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Questionário</title>
+    </head>
+    <body>
+        <fieldset>
+            <legend>Formulário</legend>
+            <form>
+                <table>
+                    <tr>
+                        <th>Nome:</th>
+                        <th><input type="text"></th>
+                    </tr>
+                    <tr>
+                        <th>Idade:</th>
+                        <th><input type="text"></th>
+                    </tr>
+                    <tr>
+                        <th>Endereço:</th>
+                        <th><input type="text"></th>
+                        <th>Nº</th>
+                        <th><input type="number"></th>
+                    </tr>
+                    <tr>
+                        <th>CEP:</th>
+                        <th><input type="text"></th>
+                    </tr>
+                </table>
+            </form>
+        </fieldset>
+    </body>
+</html>
